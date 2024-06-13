327c42180fcbeb99e26640e5d862201215f8aee1
jorkam2000 <jorkam2000@gmail.com>
2024-06-13 19:06:46 +0100

    Specialization endpoints

--- backend/api/routes/index.js
+++ backend/api/routes/index.js
@@ -3,9 +3,11 @@ const router = require("express").Router(); // Instancia de router de express pa
 const userRouter = require("./user.router");
 const insuranceRouter = require("./insurance.router");
 const patientRouter = require("./patient.router");
+const specializationRouter = require("./specialization.router");
 
 router.use("/user", userRouter);
 router.use("/insurance", insuranceRouter);
 router.use("/patient", patientRouter);
+router.use("/specialization", specializationRouter);
 
 module.exports = router; // Exporto la instancia de este router para poder importarlo en el index.js principal
