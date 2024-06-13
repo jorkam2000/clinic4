a7e9563efe86336a06d463ce816d1e9f151f049f
jorkam2000 <jorkam2000@gmail.com>
2024-06-13 20:29:37 +0100

    Doctor data endpoints

--- backend/api/routes/index.js
+++ backend/api/routes/index.js
@@ -4,10 +4,12 @@ const userRouter = require("./user.router");
 const insuranceRouter = require("./insurance.router");
 const patientRouter = require("./patient.router");
 const specializationRouter = require("./specialization.router");
+const doctorRouter = require("./doctor_data.router");
 
 router.use("/user", userRouter);
 router.use("/insurance", insuranceRouter);
 router.use("/patient", patientRouter);
 router.use("/specialization", specializationRouter);
+router.use("/doctor", doctorRouter);
 
 module.exports = router; // Exporto la instancia de este router para poder importarlo en el index.js principal
