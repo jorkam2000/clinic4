07ea65426d4e08f1a52a94e8a3b006497fa761f2
Miguel-AMD <144833048+Miguel-AMD@users.noreply.github.com>
2024-06-22 11:12:53 +0100

    Input

---
+++ frontend/clinic/src/components/Input/Input.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Input(labelName){
+    let typeV;
+    if (labelName === 'Password'){typeV='password'}
+    else if (['First name','Last name','Email'].includes(labelName)){typeV='text'}
+    else if (labelName === 'Phone'){typeV='number'}
+
+    let id = `${labelName}_textInput`
+    if (labelName === 'Phone'){}
+    return (
+        <div>
+            <label for={id}>{labeName}</label>
+            <input class='inputCorrect' type={typeV} id={id} name={labelName} placeholder="----------"></input>
+        </div>
+    )
+}
+
+export default Input;
